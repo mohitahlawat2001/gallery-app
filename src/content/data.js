b984168a05fd4ddc92438002350484cea43ff8b5
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-09 13:24:07 +0530

    gallery app

---
+++ src/content/data.js
@@ -0,0 +1,76 @@
+export const data = [
+    {
+      "name": "My dress up darling",
+      "url": "/images/insta_1.jpg",
+      "description": "Anime My dress up darling main character",
+      "date": "2020-01-01",
+      "type": "image/jpg",
+      "size": "1000 KB",
+      "width": 1000,
+      "height": 1000
+
+      
+    },
+    {
+      "name": "Danger in my heart",
+      "url": "/images/insta_2.jpg",
+      "description": "Anime Danger in my heart female main character yamada ichika",
+      "date": "2020-01-02",
+      "type": "image/jpg",
+      "size": "1000 KB",
+      "width": 1000,
+      "height": 1000
+    },
+    {
+      "name": "Danger in my heart",
+      "url": "/images/insta_3.jpg",
+      "description": "Anime Danger in my heart main male character",
+      "date": "2020-01-03",
+      "type": "image/jpg",
+      "size": "1000 KB",
+      "width": 1000,
+      "height": 1000
+    },
+    {
+      "name": "Manga",
+      "url": "/images/insta_4.jpg",
+      "description": " Manga Danger in my heart in japanese language",
+      "date": "2020-01-04",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Danger in my heart",
+      "url": "/images/insta_5.jpg",
+      "description": " Anime Danger in my heart both main characters",
+      "date": "2020-01-05",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Oshi no ko",
+      "url": "/images/insta_6.jpg",
+      "description": "Anime Oshi no ko main character",
+      "date": "2020-01-06",
+      "type": "image/jpg",
+    },
+    {
+      "name": "High Priest",
+      "url": "/images/insta_7.jpg",
+      "description": "Anime High Priest main character",
+      "date": "2020-01-07",
+      "type": "image/jpg",
+    },
+    {
+      "name": "ichika",
+      "url": "/images/insta_8.jpg",
+      "description": "Anime Danger in my heart ",
+      "date": "2020-01-08",
+      "type": "image/jpg",
+    },
+    {
+      "name": "kubosan can't let me invisible",
+      "url": "/images/insta_9.jpg",
+      "description": "Anime kubosan can't let me invisible main character",
+      "date": "2020-01-09",
+      "type": "image/jpg",
+    }
+  ]
