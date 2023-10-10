273c9dc9c343ea580b47fd350b27127135482bdf
Tanvi <ktanvarada@gmail.com>
2023-10-10 10:47:37 +0530

    improved display of cards

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ function App() {
 
   return (
     <>
-      {showModal&&<Modal setShowModal={setShowModal} setSelectedContent={setSelectedContent} content={selectedContent} data={data}/>}
+      {showModal && (
+        <Modal setShowModal={setShowModal} setSelectedContent={setSelectedContent} content={selectedContent} data={data} />
+      )}
       <div style={styles.main}>
         <Header title="Photo Gallery" subtitle="A Photo Gallery App " />
         <div style={styles.container}>
           {data.map((image_data, index) => {
             return (
-              <div style={styles.imageContainer}
+              <div
+                style={styles.card}
                 onClick={() => {
                   setShowModal(true);
                   setSelectedContent(image_data);
                 }}
+                key={index}
               >
-                <img style={styles.image} src={image_data.url} />
+                <img style={styles.image} src={image_data.url} alt={image_data.name} />
                 <div style={styles.imageDescription}>
                   <p style={styles.imageDescriptionText}>{image_data.name}</p>
                 </div>
@@ -42,42 +46,38 @@ const styles = {
     width: '100%',
     height: '100%',
     backgroundColor: '#f0f0f0',
-    flexDirection: "column"
+    flexDirection: 'column',
   },
   container: {
     display: 'flex',
     flexWrap: 'wrap',
-    gap: 10,
-    maxWidth: "60vw"
+    gap: '10px',
+    maxWidth: '80vw',
   },
-  imageContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center',
+  card: {
+    width: 'calc(25% - 10px)', // Equal-sized cards with 10px gap
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    overflow: 'hidden',
     cursor: 'pointer',
   },
   image: {
-    width: '300px',
-    height: '300px',
+    width: '100%',
+    height: 'auto',
     objectFit: 'cover',
+    maxHeight: '200px', // Adjust the max height for images
   },
   imageDescription: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center',
-    width: '100%',
-    height: '50px',
     backgroundColor: '#000000',
-    opacity: '0.5',
+    opacity: '0.7',
+    padding: '8px',
   },
   imageDescriptionText: {
     color: '#ffffff',
     fontSize: '16px',
     fontWeight: 'bold',
+    margin: 0,
   },
-}
+};
 
 export default App;
